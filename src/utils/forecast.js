88136504d055a438b67ea1ef12882bac63900170
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2022-08-04 12:58:16 +0200

    Minor modifications

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,8 +12,11 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
 
         } else {
-            callback(undefined, body.current.weather_descriptions[0] +
-                '. It is currently ' + body.current.temperature + ' degrees out, and feels like ' + body.current.feelslike + ' degrees.' +
+            callback(
+                undefined,
+                body.current.weather_descriptions[0] +
+                '. It is currently ' + body.current.temperature +
+                '°C out, and feels like ' + body.current.feelslike + '°C.' +
                 ' Current humidity is at ' + body.current.humidity + '%.')
         }
     })
