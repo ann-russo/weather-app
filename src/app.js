accc6dbe0f96fc9a32e44cd11e8a41ea77145bfd
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2022-08-04 10:56:57 +0200

    Init commit

---
+++ src/app.js
@@ -0,0 +1,101 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const app = express()
+
+// Define paths for express configuration
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Set up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Set up static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Anastasia Urusova'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide a location!'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({
+                error: error
+            })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error: error
+                })
+            }
+            res.send({
+                address: req.query.address,
+                location: location,
+                forecast: forecastData
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search);
+    res.send({
+        products: []
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Anastasia Urusova'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is a helpful text',
+        name: 'Anastasia Urusova'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404-page', {
+        title: '404',
+        name: 'Anastasia Urusova',
+        error: 'Help article not found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404-page', {
+        title: '404',
+        name: 'Anastasia Urusova',
+        error: 'Page not found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
